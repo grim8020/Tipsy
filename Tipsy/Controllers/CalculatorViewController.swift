029e04eaa89efdb759d1aec614fd323f3e49c3cb
Shane Grim <grim8020@gmail.com>
2022-04-14 12:28:41 -0400

    Add Xcode project

---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    var tip = 0.10
+    var numberOfPeople = 1
+    var billTotal = 0.0
+    var finalResult = "0.0"
+    
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        billTextField.endEditing(true)
+        
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        sender.isSelected = true
+        
+        let buttonTitle = sender.currentTitle!
+        
+        let buttonTitleMinusPercentSign = String(buttonTitle.dropLast())
+        
+        let buttonTitleAsAnswer = Double(buttonTitleMinusPercentSign)!
+        
+        tip = buttonTitleAsAnswer/100
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+        numberOfPeople = Int(sender.value)
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let bill = billTextField.text!
+        if bill != "" {
+            billTotal = Double(bill)!
+            let result = billTotal * (1 + tip) / Double(numberOfPeople)
+            finalResult = String(format: "%.2f", result)
+        }
+        
+        
+        
+        print(tip)
+        
+        let textfieldInt: Float? = Float(billTextField.text ?? "Error")
+        
+        let billPercent =  (textfieldInt ?? 0) * Float(tip)
+        let numberOfPeople: Int? = Int(splitNumberLabel.text!)
+        
+        let billTotal = ((textfieldInt ?? 0) + billPercent) / Float(numberOfPeople ?? 0)
+        let finalResult = (String(format: "$%.2f", billTotal))
+        print(finalResult)
+        
+    
+        
+        self.performSegue(withIdentifier: "goToResults", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResults" {
+            let destinationVC = segue.destination as! ResultsViewController
+            
+            destinationVC.result = finalResult
+            destinationVC.tip = Int(tip * 100)
+            destinationVC.split = numberOfPeople
+            
+            
+        }
+    }
+    
+    
+}
